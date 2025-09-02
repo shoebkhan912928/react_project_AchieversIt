1919e9099782dca62dbaee66f5464c5f57129855
shoebkhan912928 <shoebkhan912928@gmail.com>
2025-09-02 10:45:39 +0530

    produt detai responsive

--- react_project/src/projectData/CarouselData.js
+++ react_project/src/projectData/CarouselData.js
@@ -1,45 +1,32 @@
-import First from "../images/products/boat110-1.png";
+// import First from "../images/products/boat110-1.png";
+import firsts from "../images/products/fourthSlider.png";
 import Second from "../images/products/boat131-2.png";
 import Third from "../images/products/boat131-3.png";
-import Fourth from "../images/products/boat203-1.png";
+// import Fourth from "../images/products/boat203-1.png";
+import thirteens from "../images/products/jbl760nc-1.png";
 
 export const SliderData = [
   {
        id: 1,
-        tag: "hero-product",
-        tagline: "Keep the noise out, or in. You choose.",
-        heroImage: "/images/products/jbl660nc-1.png",
-        images: First,
-        brand: "JBL",
-        title: "JBL Live 660NC",
-        info: "Wireless Over-Ear NC Headphones",
-        category: "Headphones",
-        type: "Over Ear",
-        connectivity: "Wireless",
-        finalPrice: 9999,
-        originalPrice: 14999,
-        quantity: 1,
-        ratings: 1234,
-        rateCount: 5,
-        path: "/product-details/",
-  },
-  {
-     id: 2,
-        tag: "featured-product",
-        images: Second,
-        brand: "boAt",
-        title: "boAt Rockerz 518",
-        info: "On-Ear Wireless Headphones",
-        category: "Headphones",
-        type: "On Ear",
-        connectivity: "Wireless",
-        finalPrice: 1299,
-        originalPrice: 3990,
-        quantity: 1,
-        ratings: 1321,
-        rateCount: 5,
-        path: "/product-details/",
-  },
+              tag: "hero-product",
+              tagline: "Keep the noise out, or in. You choose.",
+              heroImage: "/images/products/jbl660nc-1.png",
+              images:firsts,
+                  
+            
+              brand: "JBL",
+              title: "JBL Live 660NC",
+              info: "Wireless Over-Ear NC Headphones",
+              category: "Headphones",
+              type: "Over Ear",
+              connectivity: "Wireless",
+              finalPrice: 9999,
+              originalPrice: 14999,
+              quantity: 1,
+              ratings: 1234,
+              rateCount: 5,
+              path: "/product-details/",
+          },
   {
        id: 3,
         tag: "hero-product",
@@ -60,19 +47,22 @@ export const SliderData = [
         path: "/product-details/",
   },
   {
-       id: 4,
-        images: Fourth,
-        brand: "boAt",
-        title: "boAt BassHeads 110",
-        info: "In-Ear Wired Earphones",
-        category: "Earphones",
-        type: "In Ear",
-        connectivity: "Wired",
-        finalPrice: 449,
-        originalPrice: 999,
-        quantity: 1,
-        ratings: 556,
-        rateCount: 4,
-        path: "/product-details/",
-  },
+          id: 8,
+          tag: "featured-product",
+          tagline:"Wireless Over-Ear NC Headphones",
+          images: thirteens,
+            
+          brand: "JBL",
+          title: "JBL Tune 760NC",
+          info: "Wireless Over-Ear NC Headphones",
+          category: "Headphones",
+          type: "Over Ear",
+          connectivity: "Wireless",
+          finalPrice: 5999,
+          originalPrice: 7999,
+          quantity: 1,
+          ratings: 755,
+          rateCount: 4,
+          path: "/product-details/",
+      }
 ];
